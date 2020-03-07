c90ca28be6a52386de3c4f6b2a834915b00ad458
Billy Huang <10646003@ntub.edu.tw>
2020-03-07 13:40:59 +0800

    3-4 - 編寫程式

---
+++ Assets/Scripts/RouletteController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RouletteController : MonoBehaviour {
+    private float _rotSpeed = 0; // 旋轉速度
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // 若點擊滑鼠就要設定旋轉速度
+        if (Input.GetMouseButtonDown(0)) {
+            this._rotSpeed = 10;
+        }
+
+        // 旋轉速度、讓輪盤旋轉
+        transform.Rotate(0, 0, this._rotSpeed);
+    }
+}
